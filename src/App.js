38da3dbfd00194a2e7f99223a2d466378f476d53
hoyeon <hoyeon94614@gmail.com>
2020-10-25 02:11:22 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>블라블라</h2>
-    </div>
-  );
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '송호연',
+  'birthday': '940614',
+  'gender': '여자',
+  'job': '대학생'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/5',
+  'name': '송호윤',
+  'birthday': '991210',
+  'gender': '여자',
+  'job': '대학생'
+}
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (<Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />);
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
